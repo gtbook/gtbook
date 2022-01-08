e02244af00decba465cea64c88e5ee8cc721997d
Frank Dellaert <dellaert@gmail.com>
2022-01-08 17:29:11 -0500

    gtbook_rename works

--- gtbook/cli.py
+++ gtbook/cli.py
@@ -3,13 +3,25 @@
 __all__ = ['rename', 'add_copyright']
 
 # Cell
+from pathlib import Path
 from fastcore.script import call_parse
 
 # Cell
 @call_parse
-def rename():
+def rename(dir: str  # dir in which to rename files
+           ):
     """Rename notebooks to base 1 for chapters."""
+    path = Path(dir)
+    assert path.exists(), f"dir '{dir}' not found"
+    assert path.is_dir(), f"'{dir}' is not a directory"
     print("Renaming notebooks now.")
+    nbs = [x for x in path.iterdir() if x.suffix == '.ipynb']
+    for ch in list(range(8, -1, -1)):
+        print(f"Renaming chapter {ch}:")
+        for x in path.glob(f"S{ch}*.ipynb"):
+            new_name = path / x.name.replace(f"S{ch}", f"S{ch+1}")
+            print(f"Renaming {x} to {new_name}")
+            x.rename(new_name)
 
 
 # Cell
